4a43fd5e5ae7b0d489b202a90eec3fe97af46169
Arnold Rispress <arnold.rispress@gmail.com>
2023-06-02 15:07:13 -0400

    updated code to show address list. misread instructions previously

--- Program.cs
+++ Program.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 public class Contact
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
 
     public string FullName => $"{FirstName} {LastName}";
 }
@@ -15,23 +15,21 @@ public class AddressBook
 {
     private Dictionary<string, Contact> _contactList { get; set; } = new Dictionary<string, Contact>();
 
-    public void AddContact(Contact contact)
+    public void AddContact(Contact? contact)
     {
         if (_contactList.ContainsKey(contact.Email))
         {
-            throw new ArgumentException("Contact already exists in the address book.");
+            Console.WriteLine($"A contact with that email already exists.");
         }
-
-        _contactList.Add(contact.Email, contact);
+        else
+        {
+          _contactList.Add(contact.Email, contact); 
+        }
+        
     }
 
     public Contact GetByEmail(string email)
     {
-
-        if (!_contactList.ContainsKey(email))
-        {
-            throw new ArgumentException("Contact not found in the address book.");
-        }
         return _contactList[email];
     }
 }
@@ -50,23 +48,29 @@ class Program
 
     static void Main(string[] args)
     {
-        try
-        {
-        // Create a few _contactList
-            Contact bob = new Contact() {
-            FirstName = "Bob", LastName = "Smith",
-            Email = "bob.smith@email.com",
-            Address = "100 Some Ln, Testville, TN 11111"
+        
+        
+            // Create a few _contactList
+            Contact bob = new Contact()
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Email = "bob.smith@email.com",
+                Address = "100 Some Ln, Testville, TN 11111"
             };
-            Contact sue = new Contact() {
-            FirstName = "Sue", LastName = "Jones",
-            Email = "sue.jones@email.com",
-            Address = "322 Hard Way, Testville, TN 11111"
+            Contact sue = new Contact()
+            {
+                FirstName = "Sue",
+                LastName = "Jones",
+                Email = "sue.jones@email.com",
+                Address = "322 Hard Way, Testville, TN 11111"
             };
-            Contact juan = new Contact() {
-            FirstName = "Juan", LastName = "Lopez",
-            Email = "juan.lopez@email.com",
-            Address = "888 Easy St, Testville, TN 11111"
+            Contact juan = new Contact()
+            {
+                FirstName = "Juan",
+                LastName = "Lopez",
+                Email = "juan.lopez@email.com",
+                Address = "888 Easy St, Testville, TN 11111"
             };
 
 
@@ -102,16 +106,12 @@ class Program
                 Console.WriteLine($"Email: {contact.Email}");
                 Console.WriteLine($"Address: {contact.Address}");
                 }
-                catch (ArgumentException ex)
+                catch (KeyNotFoundException ex)
                 {
                     Console.WriteLine("----------------------------");
-                    Console.WriteLine($"Error: {ex.Message}");
+                    Console.WriteLine($"No contact found for that email: {email}, ya donuthole!");
                 }
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occured: {ex.Message}");
-        }
     }
 }
+
